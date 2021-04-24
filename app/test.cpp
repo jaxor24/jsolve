ae410386b49c97f5fa2cc835d90986397535ada7
Jack <jaxor@bigpond.net.au>
2021-04-24 18:29:36 +1000

    misc
    test in app code

--- app/test.cpp
+++ app/test.cpp
@@ -1,11 +1,20 @@
 #include "test.h"
 
 #include "logging.h"
+
+#include "jsolve.h"
+
 #include "matrix.h"
 
 void do_work()
 {
 	using Matr = Matrix<double>;
+
+	auto v = jsolve::Variable(jsolve::Variable::Type::LINEAR, 0.0);
+	auto c = jsolve::Constraint(jsolve::Constraint::Type::LESS);
+
+	v.cost() = 6;
+	c.rhs() = 6;
 
 	Matr lhs{ 1, 3, 3.0 };
 	Matr rhs{ 3, 1, 4.0 };
---
+++ lib/jsolve.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include "variable.h"
+#include "constraint.h"
