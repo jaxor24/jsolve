495989b99c304be8537669740687d42880b700a2
Jack <jaxor@bigpond.net.au>
2021-04-24 18:29:18 +1000

    test adding matrices

--- jsolver/test.cpp
+++ jsolver/test.cpp
@@ -9,9 +9,9 @@ void do_work()
 
 	Mat m0{ 5, 5 };
 	Mat m1{ 3, 5, 10.5};
-	auto m2 = m1.transpose();
 
-	// auto m2 = m0 + m1;
+	auto m2 = m1.transpose();
+	auto m3 = m0 + m1;
 
 	log()->info("m0: {}", m0);
 	log()->info("m1: {}", m1);
