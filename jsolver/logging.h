fac0835772891c6fac2de35d9323dcff8a9c82bd
Jack <jaxor@bigpond.net.au>
2021-04-24 18:29:05 +1000

    split logging into cpp file

--- jsolver/logging.h
+++ jsolver/logging.h
@@ -1,27 +1,12 @@
 #include "spdlog/spdlog.h"
-#include "spdlog/sinks/basic_file_sink.h"
-#include "spdlog/sinks/stdout_sinks.h"
-#include "spdlog/sinks/stdout_color_sinks.h"
+#include "spdlog/fmt/ostr.h"
 
 namespace logging
 {
-	void init_logging()
-	{
-        auto console_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
-        console_sink->set_level(spdlog::level::trace);
- 
-        auto file_sink = std::make_shared<spdlog::sinks::basic_file_sink_mt>("logs/log.txt", true);
-        file_sink->set_level(spdlog::level::trace);
-
-        std::vector<spdlog::sink_ptr> sinks{ console_sink, file_sink };
-
-        auto logger = std::make_shared<spdlog::logger>("logger", sinks.begin(), sinks.end());
-        logger->set_level(spdlog::level::trace);
-        spdlog::register_logger(logger);
-	}
+	void init_logging();
 }
 
-auto log()
+inline auto log()
 {
     return spdlog::get("logger");
 }
