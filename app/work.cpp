0994c38218df1d17a806a3f05ccb5a33c6afa3e7
Jack <jaxor@bigpond.net.au>
2021-04-25 15:35:41 +1000

    Update work.cpp

--- app/work.cpp
+++ app/work.cpp
@@ -12,19 +12,22 @@ void do_work()
 
 	auto m = jsolve::Model(jsolve::Model::Sense::MIN, "Knapsack");
 
-	auto* v1 = m.make_variable(jsolve::Variable::Type::LINEAR, "count_item_a");
-	auto* v2 = m.make_variable(jsolve::Variable::Type::LINEAR, "count_item_b");
+	auto* v1 = m.make_variable(jsolve::Variable::Type::LINEAR, "ItemA");
+	auto* v2 = m.make_variable(jsolve::Variable::Type::LINEAR, "ItemB");
+	auto* v3 = m.make_variable(jsolve::Variable::Type::LINEAR, "ItemC");
 
 	// Objective
 	v1->cost() = 10;
 	v2->cost() = 20;
+	v3->cost() = 5;
 
 	// Capacity
-	auto* c1 = m.make_constraint(jsolve::Constraint::Type::LESS, "capacity");
+	auto* c1 = m.make_constraint(jsolve::Constraint::Type::LESS, "Capacity");
 	c1->rhs() = 50;
 	c1->add_to_lhs(15.0, v1);
 	c1->add_to_lhs(25.0, v2);
+	c1->add_to_lhs(10.0, v3);
 
-	log()->info("knapsack: {}", m);
+	log()->info(m);
 
 }
