b433c559aa6ecc8f2fcb92c9f2d7919c14da1382
Jack <jaxor@bigpond.net.au>
2021-05-31 15:18:45 +1000

    tests for div_elem and mult_elem

--- lib/matrix.h
+++ lib/matrix.h
@@ -431,6 +431,60 @@ Matrix<U> operator>(const Matrix<U>& lhs, double x)
 	return result;
 }
 
+// Free functions - helpers
+template <typename U>
+Matrix<U> mult_elem(const Matrix<U>& lhs, const Matrix<U>& rhs)
+{
+	// Element-wise multiplication
+
+	if ((lhs.n_cols() != rhs.n_cols()) || (lhs.n_rows() != rhs.n_rows()))
+	{
+		throw MatrixError("Cannot element-wise multiply matrices with different dimensions");
+	}
+
+	Matrix<U> result = lhs;
+
+	for (std::size_t n_row = 0; n_row < lhs.n_rows(); n_row++)
+	{
+		for (std::size_t n_col = 0; n_col < lhs.n_cols(); n_col++)
+		{
+			result(n_row, n_col) *= rhs(n_row, n_col);
+		}
+	}
+
+	return result;
+}
+
+template <typename U>
+Matrix<U> div_elem(const Matrix<U>& lhs, const Matrix<U>& rhs)
+{
+	// Element-wise division
+
+	if ((lhs.n_cols() != rhs.n_cols()) || (lhs.n_rows() != rhs.n_rows()))
+	{
+		throw MatrixError("Cannot element-wise divide matrices with different dimensions");
+	}
+
+	Matrix<U> result = lhs;
+
+	for (std::size_t n_row = 0; n_row < lhs.n_rows(); n_row++)
+	{
+		for (std::size_t n_col = 0; n_col < lhs.n_cols(); n_col++)
+		{
+			if (rhs(n_row, n_col) == 0)
+			{
+				throw MatrixError("Cannot element-wise divide by matrix with zero element");
+			}
+			else
+			{
+				result(n_row, n_col) /= rhs(n_row, n_col);
+			}
+		}
+	}
+
+	return result;
+}
+
 template <typename T>
 std::ostream& operator<<(std::ostream& os, const Matrix<T>& m)
 {
--- tests/test_matrix.cpp
+++ tests/test_matrix.cpp
@@ -2046,6 +2046,145 @@ TEST_CASE("operator>", "[matrix]")
     }
 }
 
+TEST_CASE("mult_elem", "[matrix]")
+{
+    SECTION("1x1 matrix")
+    {
+        Matr m1{ 1, 1, 6 };
+        Matr m2{ 1, 1, -2.5 };
+
+        auto result = mult_elem(m1, m2);
+
+        REQUIRE(result.n_rows() == 1);
+        REQUIRE(result.n_cols() == 1);
+        REQUIRE(result(0, 0) == 6 * -2.5);
+    }
+
+    SECTION("2x2 matrix")
+    {
+        Matr m1{ 2, 2, 0.0 };
+        m1(0, 0) = -1;
+        m1(0, 1) = -2;
+        m1(1, 0) = -3;
+        m1(1, 1) = -4;
+
+        Matr m2{ 2, 2, 0.0 };
+        m2(0, 0) = 4;
+        m2(0, 1) = 3;
+        m2(1, 0) = 2;
+        m2(1, 1) = 1;
+
+        auto result = mult_elem(m1, m2);
+
+        REQUIRE(result.n_rows() == 2);
+        REQUIRE(result.n_cols() == 2);
+        REQUIRE(result(0, 0) == -1*4);
+        REQUIRE(result(0, 1) == -2*3);
+        REQUIRE(result(1, 0) == -3*2);
+        REQUIRE(result(1, 1) == -4*1);
+    }
+
+    SECTION("1x3 vector")
+    {
+        Matr m1{ 1, 3 };
+        m1(0, 0) = 3;
+        m1(0, 1) = 9;
+        m1(0, 2) = 18;
+
+        Matr m2{ 1, 3 };
+        m2(0, 0) = -0.5;
+        m2(0, 1) = 0.5;
+        m2(0, 2) = 0.0;
+
+        auto result = mult_elem(m1, m2);
+
+        REQUIRE(result.n_rows() == 1);
+        REQUIRE(result.n_cols() == 3);
+        REQUIRE(result(0, 0) == 3 * -0.5);
+        REQUIRE(result(0, 1) == 9 * 0.5);
+        REQUIRE(result(0, 2) == 18 * 0.0);
+    }
+
+    SECTION("invalid dimensions")
+    {
+        REQUIRE_THROWS_AS(mult_elem(Matr(2, 2), Matr(1, 2)), MatrixError);
+        REQUIRE_THROWS_AS(mult_elem(Matr(2, 1), Matr(2, 3)), MatrixError);
+    }
+}
+
+TEST_CASE("div_elem", "[matrix]")
+{
+    SECTION("1x1 matrix")
+    {
+        Matr m1{ 1, 1, 6 };
+        Matr m2{ 1, 1, -2.5 };
+
+        auto result = div_elem(m1, m2);
+
+        REQUIRE(result.n_rows() == 1);
+        REQUIRE(result.n_cols() == 1);
+        REQUIRE(result(0, 0) == 6 / -2.5);
+    }
+
+    SECTION("2x2 matrix")
+    {
+        Matr m1{ 2, 2, 0.0 };
+        m1(0, 0) = -1;
+        m1(0, 1) = -2;
+        m1(1, 0) = -3;
+        m1(1, 1) = -4;
+
+        Matr m2{ 2, 2, 0.0 };
+        m2(0, 0) = 4;
+        m2(0, 1) = 3;
+        m2(1, 0) = 2;
+        m2(1, 1) = 1;
+
+        auto result = div_elem(m1, m2);
+
+        REQUIRE(result.n_rows() == 2);
+        REQUIRE(result.n_cols() == 2);
+        REQUIRE(result(0, 0) == -1.0 / 4);
+        REQUIRE(result(0, 1) == -2.0 / 3);
+        REQUIRE(result(1, 0) == -3.0 / 2);
+        REQUIRE(result(1, 1) == -4.0 / 1);
+    }
+
+    SECTION("1x3 vector")
+    {
+        Matr m1{ 1, 3 };
+        m1(0, 0) = 3;
+        m1(0, 1) = 9;
+        m1(0, 2) = 18;
+
+        Matr m2{ 1, 3 };
+        m2(0, 0) = -0.5;
+        m2(0, 1) = 0.5;
+        m2(0, 2) = 10;
+
+        auto result = div_elem(m1, m2);
+
+        REQUIRE(result.n_rows() == 1);
+        REQUIRE(result.n_cols() == 3);
+        REQUIRE(result(0, 0) == 3.0 / -0.5);
+        REQUIRE(result(0, 1) == 9.0 / 0.5);
+        REQUIRE(result(0, 2) == 18.0 / 10);
+    }
+
+    SECTION("invalid dimensions")
+    {
+        REQUIRE_THROWS_AS(div_elem(Matr(2, 2), Matr(1, 2)), MatrixError);
+        REQUIRE_THROWS_AS(div_elem(Matr(2, 1), Matr(2, 3)), MatrixError);
+    }
+
+    SECTION("invalid division by zero")
+    {
+        REQUIRE_THROWS_AS(div_elem(Matr(2, 2, 1.0), Matr(2, 2)), MatrixError);
+        REQUIRE_THROWS_AS(div_elem(Matr(2, 1, 5.0), Matr(2, 1)), MatrixError);
+    }
+}
+
+
 TEST_CASE("matrix::Range", "[matrix::Range]")
 {
     SECTION("Range()")
