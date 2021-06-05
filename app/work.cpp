a6b3646c3801590b0a3959edc51083a6eac243ed
Jack <jaxor@bigpond.net.au>
2021-06-05 12:42:51 +1000

    Update work.cpp
    spaces

--- app/work.cpp
+++ app/work.cpp
@@ -11,6 +11,9 @@ void go()
 	log()->info(model);
 
 	auto solution = jsolve::simplex::primal_solve(model);
-	if (solution) { jsolve::log_solution(info_logger(), solution.value()); }
 
+	if (solution) 
+	{ 
+		jsolve::log_solution(info_logger(), solution.value()); 
+	}
 }
