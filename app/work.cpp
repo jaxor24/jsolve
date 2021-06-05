8aa2545338aae96ccdef7fad340c9507a5dc86b1
Jack <jaxor@bigpond.net.au>
2021-06-05 12:23:22 +1000

    Update work.cpp
    log solution if present

--- app/work.cpp
+++ app/work.cpp
@@ -10,6 +10,7 @@ void go()
 	auto model = models::make_model_1();
 	log()->info(model);
 
-	jsolve::simplex::primal_solve(model);
+	auto solution = jsolve::simplex::primal_solve(model);
+	if (solution) { jsolve::log_solution(get_logger(), solution.value()); }
 
 }
