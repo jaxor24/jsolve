d3c29dceab6a8230f0d614d0800354a8f347485d
Jack <jaxor@bigpond.net.au>
2021-06-07 21:42:39 +1000

    model 3
    add third model from textbook

--- data/models.cpp
+++ data/models.cpp
@@ -79,4 +79,40 @@ namespace models
 		return m;
 	}
 
+	jsolve::Model make_model_3()
+	{
+		// Model from:
+		// Problem 2.1 from Linear Programming (2014) Vanderbei.
+
+		auto m = jsolve::Model(jsolve::Model::Sense::MAX, "P2.1 LP 2014");
+
+		auto* v1 = m.make_variable(jsolve::Variable::Type::LINEAR, "x1");
+		auto* v2 = m.make_variable(jsolve::Variable::Type::LINEAR, "x2");
+		auto* v3 = m.make_variable(jsolve::Variable::Type::LINEAR, "x3");
+		auto* v4 = m.make_variable(jsolve::Variable::Type::LINEAR, "x4");
+
+		// Objective
+		v1->cost() = 6;
+		v2->cost() = 8;
+		v3->cost() = 5;
+		v4->cost() = 9;
+
+		// Constraint 1
+		auto* c1 = m.make_constraint(jsolve::Constraint::Type::LESS, "C1");
+		c1->rhs() = 5;
+		c1->add_to_lhs(2, v1);
+		c1->add_to_lhs(1, v2);
+		c1->add_to_lhs(1, v3);
+		c1->add_to_lhs(3, v4);
+
+		// Constraint 2
+		auto* c2 = m.make_constraint(jsolve::Constraint::Type::LESS, "C2");
+		c2->rhs() = 3;
+		c2->add_to_lhs(1, v1);
+		c2->add_to_lhs(3, v2);
+		c2->add_to_lhs(1, v3);
+		c2->add_to_lhs(2, v4);
+
+		return m;
+	}
 }
--- data/models.h
+++ data/models.h
@@ -4,4 +4,5 @@ namespace models
 {
 	jsolve::Model make_model_1();
 	jsolve::Model make_model_2();
+	jsolve::Model make_model_3();
 }
