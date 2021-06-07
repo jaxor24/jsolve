6942867862764cbf3555d201c84900c095ae7c5e
Jack <jaxor@bigpond.net.au>
2021-06-07 22:36:27 +1000

    Update test_simplex.cpp
    add new test

--- tests/test_simplex.cpp
+++ tests/test_simplex.cpp
@@ -20,5 +20,14 @@ TEST_CASE("jsolve::simplex::primal_solve", "[matrix]")
 
         REQUIRE(solution.has_value());
         REQUIRE(solution.value().objective == 17.0);
+    SECTION("model 4")
+    {
+        auto model = models::make_model_4();
+        auto solution = jsolve::simplex::primal_solve(model);
+
+        REQUIRE(solution.has_value());
+        REQUIRE(solution.value().objective == 2.0);
+        REQUIRE(solution.value().variables.at("x1") == 1.0);
+        REQUIRE(solution.value().variables.at("x2") == 0.0);
     }
 }
