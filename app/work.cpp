f34cde5b78f501674ac6a77db968fba473168daf
Jack <jaxor@bigpond.net.au>
2021-07-17 13:14:44 +1000

    Update work.cpp
    remove temp code

--- app/work.cpp
+++ app/work.cpp
@@ -12,25 +12,10 @@ void go()
 	auto model = models::make_model_1();
 	log()->info(model);
 
-	Matrix<double> m1{ 3, 3, 0.0 }; 
-	m1(0, 0) = 1;
-	m1(0, 1) = 2;
-	m1(0, 2) = 3;
+	auto solution = jsolve::simplex::primal_solve(model);
 
-	m1(1, 0) = 4;
-	m1(1, 1) = 5;
-	m1(1, 2) = 6;
-
-	m1(2, 0) = 7;
-	m1(2, 1) = 8;
-	m1(2, 2) = 9;
-
-	m1.foo();
-
-	//auto solution = jsolve::simplex::primal_solve(model);
-
-	//if (solution) 
-	//{ 
-	//	jsolve::log_solution(info_logger(), solution.value()); 
-	//}
+	if (solution) 
+	{ 
+		jsolve::log_solution(info_logger(), solution.value()); 
+	}
 }
