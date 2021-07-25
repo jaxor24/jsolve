0478ff116bf5c3f1c9b2ab5e0ce9da0bdc56c524
Jack <jaxor@bigpond.net.au>
2021-07-25 15:52:15 +1000

    new high level methods to read mps

--- app/work.cpp
+++ app/work.cpp
@@ -4,18 +4,34 @@
 #include "logging.h"
 
 #include "jsolve.h"
+#include "mps.h"
 
 #include "matrix.h"
 
 void go()
 {
-	auto model = models::make_model_1();
-	log()->info(model);
+	auto model = models::make_model_7();
+	log()->debug(model);
 
 	auto solution = jsolve::simplex::primal_solve(model);
 
-	if (solution) 
-	{ 
-		jsolve::log_solution(info_logger(), solution.value()); 
+	if (solution)
+	{
+		jsolve::log_solution(info_logger(), solution.value());
+	}
+
+}
+
+void solve_mps_file(std::filesystem::path file)
+{
+	auto model = jsolve::read_mps(file);
+
+	log()->debug(model);
+
+	auto solution = jsolve::simplex::primal_solve(model);
+
+	if (solution)
+	{
+		jsolve::log_solution(info_logger(), solution.value());
 	}
 }
--- app/work.h
+++ app/work.h
@@ -1,3 +1,7 @@
 #pragma once
 
+#include <filesystem>
+
 void go();
+
+void solve_mps_file(std::filesystem::path file);
