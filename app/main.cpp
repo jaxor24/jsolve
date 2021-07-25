83ba621ac4c7ed03434332b52dd2a2b8ebcce259
Jack <jaxor@bigpond.net.au>
2021-07-25 15:53:10 +1000

    Update main.cpp
    add command line args

--- app/main.cpp
+++ app/main.cpp
@@ -1,12 +1,34 @@
 ﻿#include "logging.h"
-
+#include "CommandLine.hpp"
 #include "work.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-	logging::init_logging();
+
+	std::string mps_path;
+	std::string log_level;
+
+	{
+		CommandLine args("jsolve");
+		args.addArgument({ "-l", "--log" }, &log_level, "Log level [off, info, debug]");
+		args.addArgument({ "-m", "--mps" }, &mps_path, "Path to MPS file.");
+
+		try
+		{
+			args.parse(argc, argv);
+		}
+		catch (std::runtime_error const& e)
+		{
+			std::cout << e.what() << std::endl;
+			return -1;
+		}
+	}
+
+	logging::init_logging(log_level);
 	log()->info("Starting jsolve...");
-	go();
+
+	solve_mps_file(mps_path);
+
 	log()->info("Done.");
 	return 0;
 }
