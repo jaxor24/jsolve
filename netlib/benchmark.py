e1f0d6ed3e41e0241bf3bf24e6d422e7e1823e7d
Jack <jaxor@bigpond.net.au>
2022-04-24 18:32:27 +1000

    Python file to call the engine on each netlib mps file

---
+++ netlib/benchmark.py
@@ -0,0 +1,20 @@
+import logging
+import subprocess
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+logging.basicConfig(format="[%(levelname)s] %(message)s", level=logging.DEBUG)
+
+logger.info("Starting...")
+
+mps_directory = "netlib/mps"
+
+all_mps_files = Path(mps_directory).glob("*.mps")
+solver = Path("C:\\repos\\jsolver\\out\\build\\windows-default\\app\\jsolver_app.exe")
+
+for i, file in enumerate(all_mps_files):
+    logger.info(f"{i} {file.name}")
+    if "afiro" in file.name:
+        subprocess.run([solver, "--mps", file, "--log", "info"])
+
+logger.info("Done.")
