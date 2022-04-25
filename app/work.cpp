db1d88a78adcf8166e7ea763b5bc4f6134795df9
Jack <jaxor@bigpond.net.au>
2022-04-25 12:41:54 +1000

    Logging cleanup

--- app/work.cpp
+++ app/work.cpp
@@ -10,16 +10,16 @@
 
 void go(std::filesystem::path file)
 {
-	auto model = jsolve::read_mps(file);
+	auto model{ jsolve::read_mps(file) };
 
 	// auto model = models::make_model_7();
-	log()->debug(model.to_long_string());
 
-	auto solution = jsolve::simplex::primal_solve(model);
+	log()->info(model.to_string());
+
+	auto solution{ jsolve::simplex::primal_solve(model) };
 
 	if (solution)
 	{
 		jsolve::log_solution(info_logger(), solution.value());
 	}
-
 }
