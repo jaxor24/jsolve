e74cfc275d8f6b0e1c8db0b6a4243ff730f0a2a3
Jack <jaxor@bigpond.net.au>
2022-05-02 14:32:09 +1000

    17% performance improvement by swapping the for loop order

--- benchmarks/bench.cpp
+++ benchmarks/bench.cpp
@@ -6,17 +6,28 @@ using Matr = Matrix<double>;
 
 static void bench_jsolve(benchmark::State& state) 
 {
-    Matr m1{ 2, 2, 0.0 };
-    Matr m2{ 2, 2, 0.0 };
-
     for (auto _ : state) 
     {
+        state.PauseTiming();
+
+        auto rows = static_cast<std::size_t>(state.range(0));
+        auto cols = static_cast<std::size_t>(state.range(0));
+
+        /*std::cout << rows << "," << cols << std::endl;*/
+
+        Matr m1{ rows, cols, 1.0 };
+        Matr m2{ rows, cols, 2.0 };
+
+        state.ResumeTiming();
+
         m1 *= m2;
     }
 
+    state.SetComplexityN(state.range(0));
+
 }
 // Register the function as a benchmark
-BENCHMARK(bench_jsolve);
+BENCHMARK(bench_jsolve)->RangeMultiplier(2)->Range(1, 1 << 9)->Complexity(benchmark::oN);
 
 
 BENCHMARK_MAIN();
