d3a14a2465fdc1c9d7d0e32c9485e3bf116c0dfb
Jack <jaxor@bigpond.net.au>
2022-05-02 15:31:09 +1000

    bug fix + performance improvement in matrix.slice(), existing implementation allows indexing out of bounds, optimise loops to avoid branching

--- benchmarks/bench.cpp
+++ benchmarks/bench.cpp
@@ -6,28 +6,20 @@ using Matr = Matrix<double>;
 
 static void bench_jsolve(benchmark::State& state) 
 {
+    auto rows = static_cast<std::size_t>(state.range(0));
+    auto cols = static_cast<std::size_t>(state.range(0));
+    Matr mat{ rows, cols, 1.0 };
+
     for (auto _ : state) 
     {
-        state.PauseTiming();
-
-        auto rows = static_cast<std::size_t>(state.range(0));
-        auto cols = static_cast<std::size_t>(state.range(0));
-
-        /*std::cout << rows << "," << cols << std::endl;*/
-
-        Matr m1{ rows, cols, 1.0 };
-        Matr m2{ rows, cols, 2.0 };
-
-        state.ResumeTiming();
-
-        m1 *= m2;
+        benchmark::DoNotOptimize(mat.slice({}, { rows - 1, rows - 1 }));
     }
 
     state.SetComplexityN(state.range(0));
 
 }
 // Register the function as a benchmark
-BENCHMARK(bench_jsolve)->RangeMultiplier(2)->Range(1, 1 << 9)->Complexity(benchmark::oN);
+BENCHMARK(bench_jsolve)->RangeMultiplier(2)->Range(1, 1 << 10)->Complexity(benchmark::oN);
 
 
 BENCHMARK_MAIN();
