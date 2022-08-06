a642157f467704adc162ad605d10f5a473758613
Jack <jaxor@bigpond.net.au>
2022-08-06 13:01:07 +1000

    add test for mps parsing

---
+++ tests/test_mps.cpp
@@ -0,0 +1,113 @@
+#include "test_includes.h"
+
+#include "mps.h"
+
+#include <filesystem>
+#include <source_location>
+
+namespace
+{
+    std::filesystem::path get_mps(std::string file_name)
+    {
+        return std::filesystem::path{ std::source_location::current().file_name() }.remove_filename() / "mps_examples" / file_name;
+    }
+}
+
+TEST_CASE("jsolve::mps::read_mps", "[matrix]") 
+{
+    SECTION("model 1")
+    {
+        auto model{ jsolve::read_mps(get_mps("example1.mps")) };
+
+        SECTION("attributes")
+        {
+            REQUIRE(model.name() == "TESTPROB");
+            REQUIRE(model.get_constraints().size() == 6);
+            REQUIRE(model.get_variables().size() == 3);
+        }
+
+        auto* x1 = model.get_variable("XONE");
+        auto* y2 = model.get_variable("YTWO");
+        auto* z3 = model.get_variable("ZTHREE");
+
+        SECTION("variables")
+        {
+            REQUIRE(x1->cost() == 1);
+            REQUIRE(y2->cost() == 4);
+            REQUIRE(z3->cost() == 9);
+
+            // These are currently not set
+            REQUIRE(x1->lower_bound() == 0);
+            REQUIRE(y2->lower_bound() == 0);
+            REQUIRE(z3->lower_bound() == 0);
+
+            // These are currently not set
+            REQUIRE(x1->upper_bound() == std::numeric_limits<double>::max());
+            REQUIRE(y2->upper_bound() == std::numeric_limits<double>::max());
+            REQUIRE(z3->upper_bound() == std::numeric_limits<double>::max());
+        }
+
+        SECTION("constraints")
+        {
+            {
+                const auto* lim1 = model.get_constraint("LIM1");
+                REQUIRE(lim1->type() == jsolve::Constraint::Type::LESS);
+                REQUIRE(lim1->rhs() == 5);
+                const auto& lim1_entries = lim1->get_entries();
+                REQUIRE(lim1_entries.size() == 2);
+                REQUIRE(lim1_entries.at(x1) == 1);
+                REQUIRE(lim1_entries.at(y2) == 1);
+            }
+
+            {
+                const auto* lim2 = model.get_constraint("LIM2");
+                REQUIRE(lim2->type() == jsolve::Constraint::Type::GREAT);
+                REQUIRE(lim2->rhs() == 10);
+                const auto& lim2_entries = lim2->get_entries();
+                REQUIRE(lim2_entries.size() == 2);
+                REQUIRE(lim2_entries.at(x1) == 1);
+                REQUIRE(lim2_entries.at(z3) == 1);
+            }
+
+            {
+                const auto* myeqn = model.get_constraint("MYEQN");
+                REQUIRE(myeqn->type() == jsolve::Constraint::Type::EQUAL);
+                REQUIRE(myeqn->rhs() == 7);
+                const auto& myeqn_entries = myeqn->get_entries();
+                REQUIRE(myeqn_entries.size() == 2);
+                REQUIRE(myeqn_entries.at(y2) == -1);
+                REQUIRE(myeqn_entries.at(z3) == 1);
+            }
+        }
+
+        SECTION("bounds as constraints")
+        {
+            {
+                auto* x1_upper_bnd = model.get_constraint("BND_XONE_LEQ_4");
+                REQUIRE(x1_upper_bnd->type() == jsolve::Constraint::Type::LESS);
+                REQUIRE(x1_upper_bnd->rhs() == 4);
+                const auto& x1_upper_bnd_entries = x1_upper_bnd->get_entries();
+                REQUIRE(x1_upper_bnd_entries.size() == 1);
+                REQUIRE(x1_upper_bnd_entries.at(x1) == 1);
+            }
+
+            {
+                auto* y2_upper_bnd = model.get_constraint("BND_YTWO_LEQ_1");
+                REQUIRE(y2_upper_bnd->type() == jsolve::Constraint::Type::LESS);
+                REQUIRE(y2_upper_bnd->rhs() == 1);
+                const auto& y2_upper_bnd_entries = y2_upper_bnd->get_entries();
+                REQUIRE(y2_upper_bnd_entries.size() == 1);
+                REQUIRE(y2_upper_bnd_entries.at(y2) == 1);
+            }
+
+            {
+                auto* y2_lower_bnd = model.get_constraint("BND_YTWO_GEQ_1");
+                REQUIRE(y2_lower_bnd->type() == jsolve::Constraint::Type::GREAT);
+                REQUIRE(y2_lower_bnd->rhs() == 1);
+                const auto& y2_lower_bnd_entries = y2_lower_bnd->get_entries();
+                REQUIRE(y2_lower_bnd_entries.size() == 1);
+                REQUIRE(y2_lower_bnd_entries.at(y2) == 1);
+            }
+        }
+    }
+}
