e821f5848f07d15a120a6fb0da2d64f9d15c703c
Jack <jaxor@bigpond.net.au>
2022-09-24 14:03:20 +1000

    wip working matrix form primal simplex method

--- lib/primal_revised.cpp
+++ lib/primal_revised.cpp
@@ -17,6 +17,61 @@ namespace jsolve
 {
 using Mat = Matrix<double>;
 
+std::optional<std::size_t> choose_entering(const Mat& y_non_basic, double EPS2)
+{
+    std::optional<std::size_t> entering;
+    Mat::value_type current_min{-EPS2};
+
+    for (std::size_t curr_idx = 0; curr_idx < y_non_basic.n_rows(); curr_idx++)
+    {
+        if (y_non_basic(curr_idx, 0) < current_min)
+        {
+            entering = curr_idx;
+            current_min = y_non_basic(curr_idx, 0);
+        }
+    }
+
+    return entering;
+}
+
+std::optional<std::size_t> choose_leaving(const Mat& x_basic, const Mat& dx, double EPS1)
+{
+    std::optional<std::size_t> leaving;
+    Mat::value_type min_ratio{std::numeric_limits<Mat::value_type>::max()};
+
+    for (std::size_t curr_idx = 0; curr_idx < x_basic.n_rows(); curr_idx++)
+    {
+        if (dx(curr_idx, 0) > EPS1)
+        {
+            auto curr_ratio{x_basic(curr_idx, 0) / dx(curr_idx, 0)};
+
+            if (!min_ratio)
+            {
+                min_ratio = curr_ratio;
+            }
+            else if (curr_ratio < min_ratio)
+            {
+                min_ratio = curr_ratio;
+                leaving = curr_idx;
+            }
+        }
+    }
+
+    return leaving;
+}
+
+double primal_obj(const Mat& c, const Mat& x_basic, const std::vector<VarData>& basics)
+{
+    double primal_obj{0.0};
+
+    for (std::size_t curr_idx = 0; curr_idx < x_basic.n_rows(); curr_idx++)
+    {
+        primal_obj += c(basics[curr_idx].index, 0) * x_basic(curr_idx, 0);
+    }
+
+    return primal_obj;
+}
+
 std::optional<Solution> solve_primal_revised(const Model& model)
 {
     auto num_user_vars = model.get_variables().size();
@@ -25,9 +80,6 @@ std::optional<Solution> solve_primal_revised(const Model& model)
     auto n = num_user_vars;
     auto m = num_constr;
     auto total_vars = n + m;
-
-    // Create A with subtrices A = [N | B]
-    Locations locations;
 
     Mat A{m, total_vars, 0.0};
 
@@ -49,21 +101,25 @@ std::optional<Solution> solve_primal_revised(const Model& model)
     }
 
     // Setup the variable location data
-    for (auto index = 0; index < total_vars; index++)
+    std::vector<VarData> basics;
+    basics.reserve(n);
+    std::vector<VarData> non_basics;
+    non_basics.reserve(m);
+
+    for (int index = 0; index < total_vars; index++)
     {
-        if (index < num_user_vars)
+        if (index < n)
         {
-            locations.non_basics[index] = {
-                index, // These are the only index variables we care about
-                index, false, false};
+            non_basics.push_back(
+                {index, // These are the only index variables we care about
+                 index, false, false}
+            );
         }
         else
         {
-            locations.basics[index] = {index, index, true, false};
+            basics.push_back({index, index, true, false});
         }
     }
-
-    assert(locations.basics.size() + locations.non_basics.size() == total_vars);
 
     log()->info(A);
 
@@ -113,13 +169,89 @@ std::optional<Solution> solve_primal_revised(const Model& model)
     log()->info(N);
 
     auto x_basic = b;
-    auto y_non_basic = -c.slice({0, n - 1}, {});
+    auto z_non_basic = -c.slice({0, n - 1}, {});
 
     log()->info(x_basic);
-    log()->info(y_non_basic);
+    log()->info(z_non_basic);
+
+    // Tolerances
+    double EPS1{1e-8};  // Minimum value to consider as exiting var
+    double EPS2{1e-12}; // Protection from division by zero
+
+    if (x_basic.max() < -EPS2)
+    {
+        throw SolveError("Infeasible intial basis");
+    }
 
     // ----------------------------------------------------------------
     // Main Loop (Primal)
+    // Steps follow Figure 6.1 from Vanderbei, p92.
+
+    int max_iter{10000};
+    int iter{0};
+
+    for (; iter <= max_iter; iter++)
+    {
+        // 1. Check optimality
+        // 2. Find entering variable
+        // Pick minimum (and negative) z_non_basic
+        std::optional<std::size_t> entering = choose_entering(z_non_basic, EPS2);
+
+        if (!entering)
+        {
+            log()->info("Optimal solution found: {}", primal_obj(c, x_basic, basics));
+            break;
+        }
+
+        // 3. Calculate dx
+        // dx = inv(B) * N * ej
+        // where j = entering index
+
+        auto b_inverse_n = inverse(B) * N;
+        auto dx = b_inverse_n.slice({}, {entering.value()});
+
+        log()->debug(dx);
+
+        // 4. Find the leaving variable
+        // 5. Calculate primal step length
+        // t = x/dx
+
+        std::optional<std::size_t> leaving = choose_leaving(x_basic, dx, EPS1);
+
+        if (!leaving)
+        {
+            log()->info("Unbounded");
+            break;
+        }
+
+        auto t = x_basic(leaving.value(), 0) / dx(leaving.value(), 0);
+
+        // 6. Calculate dz
+        // dz = -1 * transpose(inv(B)*N) * ei
+        // where i = leaving index
+
+        auto dz = -1.0 * (b_inverse_n.slice({leaving.value()}, {})).make_transpose();
+        log()->debug(dz);
+
+        // 7. Calculate dual step lengths
+        // s = z/dz (j)
+        auto s = z_non_basic(entering.value(), 0) / dz(entering.value(), 0);
+
+        // 8. Update primal and dual solutions
+        x_basic = x_basic - t * dx;
+        z_non_basic = z_non_basic - s * dz;
+
+        // 9. Update variables
+
+        B.update({}, {leaving.value()}, A.slice({}, static_cast<std::size_t>(non_basics[entering.value()].index)));
+        N.update({}, {entering.value()}, A.slice({}, static_cast<std::size_t>(basics[leaving.value()].index)));
+        x_basic(leaving.value(), 0) = t;
+        z_non_basic(entering.value(), 0) = s;
+
+        log()->debug(B);
+        log()->debug(N);
+        std::swap(basics[leaving.value()], non_basics[entering.value()]);
+    }
 
     return {};
 }
