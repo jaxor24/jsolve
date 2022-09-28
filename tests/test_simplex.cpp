595a4fc9a1e705c4fab1971ec83e1aec7b353cf4
Jack <jaxor@bigpond.net.au>
2022-09-28 20:51:57 +1000

    wip - enable testing of revised simplex algorithm

--- tests/test_simplex.cpp
+++ tests/test_simplex.cpp
@@ -15,12 +15,12 @@ TEST_CASE("jsolve::solve")
         return jsolve::solve(model, jsolve::alg_type::STANDARD);
     };
 
-    // auto revised = [](jsolve::Model& model) {
-    //     return jsolve::solve(model, jsolve::alg_type::REVISED);
-    // };
+    auto revised = [](jsolve::Model& model) {
+        return jsolve::solve(model, jsolve::alg_type::REVISED);
+    };
 
     auto [current_alg, alg_str] = GENERATE_COPY(
-        as<test_data>{}, std::make_pair(standard, "Standard Simplex") // , std::make_pair(revised, "Revised Simplex")
+        as<test_data>{}, std::make_pair(standard, "Standard Simplex"), std::make_pair(revised, "Revised Simplex")
     );
 
     SECTION("model 1")
