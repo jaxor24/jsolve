0be94d7fd427af4fa72421cf7d1c42e8e9a622b3
Jack <jaxor@bigpond.net.au>
2022-12-08 15:01:08 +1000

    Get python runner working

--- netlib/benchmark.py
+++ netlib/benchmark.py
@@ -10,7 +10,7 @@ logger.info("Starting...")
 mps_directory = "netlib/mps"
 
 all_mps_files = Path(mps_directory).glob("*.mps")
-solver = Path("C:\\repos\\jsolver\\out\\build\\windows-release\\app\\jsolver_app.exe")
+solver = Path("C:\\repos\\jsolve\\out\\build\\windows-release\\app\\jsolver_app.exe")
 
 for i, file in enumerate(all_mps_files):
     logger.info(f"{i} {file.name}")
